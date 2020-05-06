90500461edaab41c664bba1b3fda49db57dade5c
endysuryo <endysuryalaksana@gmail.com>
2020-05-06 10:47:47 +0700

    Add service example using mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 // Load dot environment before load other modules
 import dotenv = require('dotenv');
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TimeoutInterceptor } from './timeout.interceptor';
 import { ExampleModule } from './example/example.module';
+import { CatsModule } from './cats/cats.module';
 import * as migrations from './migrations/index';
 
 const { parsed } = dotenv.config({
@@ -20,28 +22,8 @@ process.env = { ...process.env, ...parsed };
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.TYPEORM_HOST,
-      password: process.env.TYPEORM_PASSWORD,
-      username: process.env.TYPEORM_USERNAME,
-      database: process.env.TYPEORM_DATABASE,
-      port: Number(process.env.TYPEORM_PORT),
-      entities: [
-        __dirname + '/**/*.entity{.ts,.js}',
-        __dirname + '/**/**/*.entity{.ts,.js}',
-        __dirname + '/**/**/**/*.entity{.ts,.js}',
-      ],
-      logging: Boolean(process.env.TYPEORM_LOGGING),
-      synchronize: false,
-      migrationsRun: true,
-      dropSchema: false,
-      cli: {
-        migrationsDir: __dirname + '/migrations',
-      },
-      migrations: [migrations.InitDB1584697797169],
-    }),
-    ExampleModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [
